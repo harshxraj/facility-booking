381f0af4660b19ce1d5136ea299136811b627e6c
harshxraj <harshraj0588@gmail.com>
2024-05-24 16:05:23 +0530

    done

--- FacilityBookingSystem.js
+++ FacilityBookingSystem.js
@@ -1,49 +1,14 @@
-class Facility {
-  constructor(name, rates) {
-    this.name = name;
-    this.rates = rates;
-    this.bookings = {};
-  }
-
-  isAvailable(startTime, endTime) {
-    for (let time = startTime; time < endTime; time++) {
-      if (this.bookings[time]) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  book(startTime, endTime) {
-    for (let time = startTime; time < endTime; time++) {
-      this.bookings[time] = true;
-    }
-  }
-
-  calculateAmount(startTime, endTime) {
-    let amount = 0;
-    if (this.name === "Clubhouse") {
-      for (let time = startTime; time < endTime; time++) {
-        if (time >= 10 && time < 16) {
-          amount += this.rates.day;
-        } else if (time >= 16 && time < 22) {
-          amount += this.rates.evening;
-        }
-      }
-    } else {
-      amount = (endTime - startTime) * this.rates.hour;
-    }
-    return amount;
-  }
-}
-
 class FacilityBookingSystem {
   constructor() {
     this.facilities = {};
   }
 
   addFacility(name, rates) {
-    this.facilities[name] = new Facility(name, rates);
+    this.facilities[name] = {
+      name,
+      rates,
+      bookings: {},
+    };
   }
 
   bookFacility(bookingInput) {
@@ -64,12 +29,29 @@ class FacilityBookingSystem {
       return "Date must be greater than the current Date.";
     }
 
-    if (!facility.isAvailable(startTime, endTime)) {
-      return `Booking Failed, Already Booked`;
+    for (let time = startTime; time < endTime; time++) {
+      if (facility.bookings[time]) {
+        return "Booking Failed, Already Booked";
+      }
     }
 
-    facility.book(startTime, endTime);
-    const amount = facility.calculateAmount(startTime, endTime);
+    for (let time = startTime; time < endTime; time++) {
+      facility.bookings[time] = true;
+    }
+
+    let amount = 0;
+    if (facility.name === "Clubhouse") {
+      for (let time = startTime; time < endTime; time++) {
+        if (time >= 10 && time < 16) {
+          amount += facility.rates.day;
+        } else if (time >= 16 && time < 22) {
+          amount += facility.rates.evening;
+        }
+      }
+    } else {
+      amount = (endTime - startTime) * facility.rates.hour;
+    }
+
     return `Booked, Rs. ${amount}`;
   }
 }
